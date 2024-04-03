44e9cfd6619598052206cabb4ec4cabc491907b4
guscostam <guscostam@live.com>
2024-04-03 20:03:15 -0300

    Atualizando banco dados

--- data/database.py
+++ data/database.py
@@ -7,19 +7,17 @@ cursor = conn.cursor()
 cursor.execute('''CREATE TABLE IF NOT EXISTS clients (
                     id INTEGER PRIMARY KEY,
                     name TEXT,
+                    lastname TEXT,
                     email TEXT,
                     password TEXT,
-                    phonenumber TEXT,
-                    addres TEXT
+                    confirmpassword TEXT
                 )''')
 
 clients = [
-    ('João Silva', 'joao@email.com', 'senha1', '123456789', 'Rua A, 123'),
-    ('Maria Santos', 'maria@email.com', 'senha2', '987654321', 'Avenida B, 456'),
-    ('Pedro Oliveira', 'pedro@email.com', 'senha3', '456789123', 'Rua C, 789')
+    ('Gustavo', 'Margotti', 'guscostam@gmail.com', '12345', '12345')
 ]
 
-cursor.executemany("INSERT INTO clients (name, email, password, phonenumber, addres) VALUES (?, ?, ?, ?, ?)", clients)
+cursor.executemany("INSERT INTO clients (name, lastname, email, password, confirmpassword) VALUES (?, ?, ?, ?, ?)", clients)
 
 conn.commit()
 conn.close()
