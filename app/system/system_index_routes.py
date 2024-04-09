65df305d4ab30766a8cfeebd88067d987559fbc1
guscostam <guscostam@live.com>
2024-04-08 22:36:29 -0300

    System routes

---
+++ app/system/system_index_routes.py
@@ -0,0 +1,17 @@
+from flask import Blueprint, request, render_template, redirect, url_for, session
+
+system_index_bp = Blueprint('system_index', __name__)
+
+@system_index_bp.route('/signup-clients', methods=['GET', 'POST'])
+def signup_clients():
+    if 'username' in session:
+        if request.method == 'POST':
+            name = request.form['first_name']
+            last_name = request.form['last_name']
+            email = request.form['email']
+            cel = request.form['phone']
+            addres = request.form['addres']
+
+        return render_template('signup_clients.html')
+    else:
+        return redirect(url_for('login.index_login'))
